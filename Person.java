186491f323b740449c695c0c40f4086fa8c6f8a8
Qurban Qurbanov <qurbanovq0612@gmail.com>
2026-02-27 09:16:41 +0400

    added exercise 8

---
+++ Person.java
@@ -0,0 +1,35 @@
+public class Person {
+
+    String firstName;
+    String lastName;
+    String gender;
+
+
+    public Person(String firstName, String lastName, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getGender() { return gender; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setGender(String gender) { this.gender = gender; }
+
+
+    public String toString() {
+        return firstName + " " + lastName + ", Gender: " + gender;
+    }
+
+
+    public boolean equals(Person p) {
+        return p != null &&
+               firstName.equals(p.firstName) &&
+               lastName.equals(p.lastName) &&
+               gender.equals(p.gender);
+    }
+}
---
+++ Teacher.java
@@ -0,0 +1,35 @@
+public class Teacher extends Person {
+
+    String department;
+    String[] courses;
+
+    public Teacher(String firstName, String lastName, String gender,
+                   String department, String[] courses) {
+        super(firstName, lastName, gender);
+        this.department = department;
+        this.courses = courses;
+    }
+
+    public String getDepartment() { return department; }
+    public String[] getCourses() { return courses; }
+
+    public void setDepartment(String department) { this.department = department; }
+    public void setCourses(String[] courses) { this.courses = courses; }
+
+    public String toString() {
+        String list = "";
+        for (String c : courses) list += c + " ";
+        return super.toString() + ", Dept: " + department + ", Courses: " + list;
+    }
+
+    public boolean equals(Teacher t) {
+        if (t == null) return false;
+        if (!super.equals(t)) return false;
+        if (!department.equals(t.department)) return false;
+        if (courses.length != t.courses.length) return false;
+        for (int i = 0; i < courses.length; i++) {
+            if (!courses[i].equals(t.courses[i])) return false;
+        }
+        return true;
+    }
+}
